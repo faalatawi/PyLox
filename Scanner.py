8f279ffc00d5c0e30fc29685b55b52908b0760c0
faalatawi <fa.atawi@gmail.com>
2020-02-05 16:24:46 -0700

    push

--- Scanner.py
+++ Scanner.py
@@ -19,7 +19,15 @@ class Scanner(object):
             "-" : TokenType.MINUS,
             "+" : TokenType.PLUS,
             ";" : TokenType.SEMICOLON,
-            "*" : TokenType.STAR
+            "*" : TokenType.STAR,
+            "!" : TokenType.BANG,
+            "!=" : TokenType.BANG_EQUAL,
+            "=" : TokenType.EQUAL,
+            "==" : TokenType.EQUAL_EQUAL,
+            "<" : TokenType.LESS,
+            "<=" : TokenType.LESS_EQUAL,
+            ">" : TokenType.GREATER,
+            ">=" : TokenType.GREATER_EQUAL 
         }
 
     def advance(self):
@@ -31,12 +39,96 @@ class Scanner(object):
         text = self.source[self.start : self.current]
         t = Token(type, text, literal, self.line)
         self.tokens.append(t)
+    
+    def match(self, expected):
+        if self.isAtEnd() :
+            return False
+        elif self.source[self.current] != expected :
+            return False
+        
+        self.current += 1
+        return True
+
+    def peek(self):
+        if self.isAtEnd() :
+            return "\0"
+        return self.source[self.current]
+
+    def string(self):
+        while self.peek() != '"' and not self.isAtEnd():
+            if self.peek() == "\n":
+                self.line += 1
+            self.advance()
+        
+        #Unterminated string.                                 
+        if self.isAtEnd() :                                         
+            lox_error(self.line, "Unterminated string.")              
+            return
+
+        # The closing ".
+        self.advance()
+
+        start = self.start + 1
+        end = self.current - 1
+        value = self.source[start:end] #TODO
+
+        self.addToken(TokenType.STRING, value)                                              
+    
+    def isDigit(self, c):
+        return '0' <= c <= '9'
+    
+    def peekNext(self):
+        next = self.current + 1
+        if next >= len(self.source):
+            return '\0'
+        return self.source[next]
+
+    def number(self):
+        while self.isDigit(self.peek()) :
+            self.advance()
+        
+        if self.peek() == '.' and self.isDigit(self.peekNext()):
+            # Consume the "."
+            self.advance()
+        
+        while self.isDigit(self.peek()) :
+            self.advance()
+        
+        start = self.start 
+        end = self.current 
+        value = self.source[start:end] #TODO
+
+        value = float(value)
+        self.addToken(TokenType.NUMBER, value)
+
 
     def scanToken(self):
         c = self.advance()
 
         if c in self.token_dic:
+            if c in ['!', '=', '<', '>'] and self.match("="):
+                c += '='
             self.addToken(self.token_dic[c])
+
+        elif c == "/":
+            if self.match("/") :
+                while self.peek() != "\n" and not self.isAtEnd():
+                    self.advance()
+            else:
+                self.addToken(TokenType.SLASH)
+        
+        elif c in ['\r', ' ', '\t']:
+            pass
+
+        elif c == '\n':
+            self.line += 1
+
+        elif c == '"':
+            self.string()
+
+        elif self.isDigit(c):
+            self.number()
+
         else:
             lox_error(self.line, "Unexpected character.")
     
