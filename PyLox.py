6c981f74b226ae67fde71cddba797cef879d3ad2
faalatawi <fa.atawi@gmail.com>
2020-02-05 12:08:23 -0700

    Feb 5

---
+++ PyLox.py
@@ -0,0 +1,58 @@
+"""
+Faisal Alatawi
+Feb, 2020
+
+"""
+import sys
+from termcolor import colored
+from Scanner import lox_scanner
+
+# Global verabiles
+# ++++++++++++++++++++++
+HAD_ERROR = True
+# ++++++++++++++++++++++
+
+
+def report(line, where, message):
+    out = "[line " + line + "] Error" + where + ": " + message
+    print(colored(out, 'red'))
+
+
+def error(line, message):
+    report(line, "", message)
+
+
+def run(source):
+    scanner = lox_scanner(source)
+    tokens = scanner.scanTokens()
+
+    for token in tokens:
+        print(token)
+
+
+def runFile(path):
+    f = open(path, "r")
+    source = f.read()
+
+    run(source)
+
+    if HAD_ERROR:
+        exit(65)
+
+
+def runPrompt():
+    while True:
+        user_input = input("> ")
+        run(user_input)
+
+
+if __name__ == "__main__":
+    arg_length = len(sys.argv)
+    if arg_length > 3:
+        print("Usage: PyLox [script]")
+        exit(2)
+    elif arg_length == 3:
+        path = sys.argv[2]
+        runFile(path)
+    else:
+        runPrompt()
---
+++ Scanner.py
@@ -0,0 +1,9 @@
+class lox_scanner(object):
+    def __init__(self, source):
+        self.test = True
+
+    def funcname(self, source):
+        pass
+
+    def scanTokens(self):
+        return []
