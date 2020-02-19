c1ff3e867935a9bb0137f96e349d64876e22e619
faalatawi <fa.atawi@gmail.com>
2020-02-19 13:50:06 -0700

    feb 19

---
+++ lox/interpreter.py
@@ -0,0 +1,6 @@
+# Copyright (c) 2020 Faisal Alatawi. All rights reserved
+# Using this source code is governed by an MIT license
+# you can find it in the LICENSE file.
+
+def faisal():
+    print("faisal")
---
+++ testing/interpreter_test.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2020 Faisal Alatawi. All rights reserved
+# Using this source code is governed by an MIT license
+# you can find it in the LICENSE file.
+
+from lox.interpreter import faisal
+
+# if __name__ == "__main__":
+faisal()
