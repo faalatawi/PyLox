7bb9592b734fb0da3d64a019fd7217eac6e067ed
faalatawi <fa.atawi@gmail.com>
2020-02-19 18:10:11 -0700

    interpreter

--- lox/interpreter.py
+++ lox/interpreter.py
@@ -2,3 +2,89 @@
 # Using this source code is governed by an MIT license
 # you can find it in the LICENSE file.
 
+from . import grammer
+from .token_type import TokenType
+
+class Interpreter(grammer.VisitorInterface):
+    def __init__(self):
+        print("test")
+    def evaluate(self, expr):
+        return expr.accept(self)
+
+    def visitLiteral(self, expr : grammer.Literal):
+        return expr.value 
+    
+    def visitGrouping(self, expr : grammer.Grouping):
+        return self.evaluate(expr.expression)
+    
+    def visitUnary(self, expr : grammer.Unary):
+        right = self.evaluate(expr.right)
+
+        if expr.operator == TokenType.MINUS :
+            return -1 * float(right)
+        
+        if expr.operator == TokenType.BANG :
+            return not self._is_truthy(right)
+
+        # Unreachable
+        return None
+    
+    
+    def visitBinary(self, expr : grammer.Binary):
+        left = self.evaluate(expr.left)
+        right = self.evaluate(expr.right)
+
+        if expr.operator == TokenType.MINUS:
+            return float(left) - float(right)
+        
+        if expr.operator == TokenType.SLASH :
+            return float(left) / float(right)
+        
+        if expr.operator == TokenType.STAR :
+            return float(left) * float(right)
+
+        if expr.operator == TokenType.PLUS :
+            if ( type(left) == float ) and ( type(right) == float ):
+                return left + right
+
+            if ( type(left) == str ) and ( type(right) == str ):
+                return left + right
+        
+        if expr.operator == TokenType.GREATER :
+            return float(left) > float(right)
+
+        if expr.operator == TokenType.GREATER_EQUAL :
+            return float(left) >= float(right)
+
+        if expr.operator == TokenType.LESS :
+            return float(left) < float(right)
+        
+        if expr.operator == TokenType.LESS_EQUAL :
+            return float(left) <= float(right)
+        
+        if expr.operator == TokenType.BANG_EQUAL :
+            return not self._is_equal(left, right)
+        
+        if expr.operator == TokenType.EQUAL_EQUAL :
+            return self._is_equal(left, right)
+
+        return None
+
+
+    # ======
+    def _is_truthy(self, expr):
+        if expr == None:
+            return False
+        
+        if type(expr) == bool :
+            return expr
+        
+        return True
+    
+    def _is_equal(self, left, right):
+        # NO NEED: 
+        # if left == None and right == None:
+        #     return True
+        # if left == None :
+        #     return False 
+        return left == right    
--- testing.py
+++ testing.py
@@ -1,3 +1,19 @@
 # Copyright (c) 2020 Faisal Alatawi. All rights reserved
 # Using this source code is governed by an MIT license
 # you can find it in the LICENSE file.
+
+
+def test_interpreter():
+    from lox import interpreter
+    interpreter.Interpreter()
+
+
+if __name__ == "__main__":
+    # test_interpreter()
+
+    a = 'ala'
+    b = 'ala'
+
+    print(float(a))
+
+
